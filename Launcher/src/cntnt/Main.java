057bef03ee25b371ae8c4d68fb0a9bbd03080d66
Svarog36 <grundmann-sebastian@t-online.de>
2021-06-13 14:53:24 +0200

    Added JavaFX debug info

--- Launcher/src/cntnt/Main.java
+++ Launcher/src/cntnt/Main.java
@@ -46,7 +46,16 @@ public class Main extends Application {
     }
 
 
+    /*
 
+    if Error: JavaFX runtime components are missing, and are required to run this application
+
+    add this to VM Options in Run/Debug Configurations
+
+    --module-path "PATH_TO\javafx-sdk-15.0.1\lib" --add-modules javafx.fxml,javafx.controls
+
+    example: --module-path "B:\Programmieren\javafx-sdk-15.0.1\lib" --add-modules javafx.fxml,javafx.controls
+     */
     
 
 }
